7ba375203fc084d666262721b844a4dc519f24b6
Jaden1441 <Jaden.franks@gmail.com>
2026-02-19 14:39:47 -0500

    third commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
+    import argparse
     import os 
     from dotenv import load_dotenv
+    from google.genai import types
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,9 +12,15 @@ def main():
 
     from google import genai
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     
     client = genai.Client(api_key = api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum." )
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",contents=messages
+        )
     
     usage = response.usage_metadata
     prompt_tokens = usage.prompt_token_count
