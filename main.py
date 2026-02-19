29502590bb544b57264d5695029db9e95c1f7d61
Jaden1441 <Jaden.franks@gmail.com>
2026-02-19 10:20:15 -0500

    token count

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ def main():
     
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(model="gemini-2.5-flash",contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum." )
-    print(response.text)
     
+    usage = response.usage_metadata
+    prompt_tokens = usage.prompt_token_count
+    response_tokens = usage.candidates_token_count
+
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print(f"\n{response.text}")
+    
+
 if __name__ == "__main__":
     main()
