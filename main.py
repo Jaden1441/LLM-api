dcb9503bf5530c5f8d84dde28c9f38ccc05450ed
Jaden1441 <Jaden.franks@gmail.com>
2026-02-25 13:31:47 -0500

    adding verbose

--- main.py
+++ main.py
@@ -14,22 +14,27 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose",action="store_true", help="Enable verbose output")
+    parser.add_argument("--prompt", required=False)
     args = parser.parse_args()
     
+
     client = genai.Client(api_key = api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model="gemini-2.5-flash",contents=messages
         )
-    
+
     usage = response.usage_metadata
     prompt_tokens = usage.prompt_token_count
     response_tokens = usage.candidates_token_count
 
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
-    print(f"\n{response.text}")
-    
+    if args.verbose:
+        print(f"User prompt:{args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(f"{response.text}")    
 
 if __name__ == "__main__":
     main()
